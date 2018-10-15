b20049654e09b53858a967e01509c8df6747d139
Rithy Thul <rithythul@users.noreply.github.com>
2018-10-15 18:08:41 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -4,6 +4,6 @@ sudo apt install espeak -y;
 
 espeak "Welcome to Koom Pee."
 espeak "Your Koom-Pee is secured"
-
+espeak "Do you want to re-assured?"
 #To be listed new updated.
 echo "success" > up.text
